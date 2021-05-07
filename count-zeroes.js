66a7224461b8434035f80438d42edb0f3d2a3de8
Brandon McIntosh <brandon.l.mcintosh@gmail.com>
2021-05-07 16:51:41 -0700

    working

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,21 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+	const firstZero = findFirstZero(arr);
+	if (firstZero === -1) return 0;
+	const numZeros = arr.length - firstZero;
+	return numZeros;
 }
 
-module.exports = countZeroes
+const findFirstZero = (arr, beg = 0, end = arr.length - 1) => {
+	if (end >= beg) {
+		const mid = beg + Math.floor((end - beg) / 2);
+		if ((mid === 0 && arr[mid] === 0) || (arr[mid - 1] === 1 && arr[mid] === 0)) {
+			return mid;
+		} else if (arr[mid] === 1) {
+			return findFirstZero(arr, mid + 1, end);
+		}
+		return findFirstZero(arr, beg, mid - 1);
+	}
+	return -1;
+};
+
+module.exports = countZeroes;
--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,5 +1,34 @@
-function sortedFrequency() {
-
+function sortedFrequency(arr, num) {
+	const firstOccurrence = findFirstOccurrence(arr, num);
+	const findLastOccurrence = findLastOccurrence(arr, num);
+	if (firstOccurrence === -1) return 0;
+	const numZeros = arr.length - firstOccurrence;
+	return numZeros;
 }
 
-module.exports = sortedFrequency
+const findFirstOccurrence = (arr, num, beg = 0, end = arr.length - 1) => {
+	if (end >= beg) {
+		const mid = beg + Math.floor((end - beg) / 2);
+		if ((mid === 0 && arr[mid] === num) || (arr[mid - 1] !== num && arr[mid] === num)) {
+			return mid;
+		} else if (arr[mid] !== num) {
+			return findFirstOccurrence(arr, num, mid + 1, end);
+		}
+		return findFirstOccurrence(arr, num, beg, mid - 1);
+	}
+	return -1;
+};
+
+const findLastOccurrence = (arr, num, beg = 0, end = arr.length - 1) => {
+	if (end >= beg) {
+		const mid = beg + Math.floor((end - beg) / 2);
+		if ((mid === 0 && arr[mid] === num) || (arr[mid - 1] !== num && arr[mid] === num)) {
+			return mid;
+		} else if (arr[mid] !== num) {
+			return findFirstOccurrence(arr, num, mid + 1, end);
+		}
+		return findFirstOccurrence(arr, num, beg, mid - 1);
+	}
+	return -1;
+};
+module.exports = sortedFrequency;
